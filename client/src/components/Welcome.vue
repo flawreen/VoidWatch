71f7d8cc8b2c2c04f69ef1cbfe43ae6fccfb59cc
craciun.florin1512 <craciun.florin1512@gmail.com>
2025-12-02 21:54:38 +0200

    [#2] init vue3 composition api with vuetify

---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,37 @@
+<template>
+	<v-container class="fill-height" max-width="900">
+		<div>
+			<v-row>
+				<v-col cols="12">
+					<v-card
+						class="py-4"
+						color="surface-variant"
+						image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+						prepend-icon="mdi-rocket-launch-outline"
+						rounded="lg"
+						variant="tonal"
+					>
+
+						<template #title>
+							<h2 class="text-h2 font-weight-bold">
+								VoidWatch
+							</h2>
+						</template>
+
+						<template #subtitle>
+							<h4 class="text-h4 font-weight-regular">
+
+							</h4>
+						</template>
+
+					</v-card>
+				</v-col>
+
+			</v-row>
+		</div>
+	</v-container>
+</template>
+
+<script setup>
+	//
+</script>
